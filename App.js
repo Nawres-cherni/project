10412f10ffa1977ca0cd9b605aafb212df29e44c
Saif <cherninawress22@gmail.com>
2020-02-20 18:17:00 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { View ,Text} from 'react-native';
+
+
+
+class App extends Component {
+    render() {
+        return (
+          <View>
+            <Text>
+              hii
+            </Text>
+          </View>
+        );
+    }
+}
+
+export default App;
